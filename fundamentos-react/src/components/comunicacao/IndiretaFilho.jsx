4e0df1c352c01c2719fdc32eb0a18f56c16b2a58
sauloeniefi <sauloeniefi@gmail.com>
2020-11-09 22:10:29 -0300

    useState hook

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default (props) => {
   const callback = props.quandoClicar;
+  const min = 50;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * 20) + 50;
+  const gerarNerd = () => Math.random() > 0.5;
   return (
     <div>
       <div>Filho</div>
-      <button
-        onClick={(e) => {
-          callback("Aldino", 46, true);
-        }}
-      >
+      <button onClick={(_) => callback("Aldino", gerarIdade, gerarNerd)}>
         Fornecer informações
       </button>
     </div>
